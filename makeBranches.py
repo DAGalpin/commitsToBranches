cc662bf6f933e3e26228fd36158e091acb3e24d2
Dan Galpin <dgalpin@google.com>
2018-09-03 04:22:51 -0700

    Fixed parsing for when steps have messages.  Added test and verbose modes.

--- makeBranches.py
+++ makeBranches.py
@@ -1,4 +1,4 @@
-#! /usr/local/bin/python
+#! /usr/bin/python
 
 from git import Repo
 import os
@@ -16,7 +16,7 @@ IS_BRANCH_STRING = "Step"
 
 MAX_LENGTH = 100
 
-def makeBranches(repo_directory, branch_code_steps):
+def makeBranches(repo_directory, branch_code_steps, verbose, test):
     print repo_directory
     print branch_code_steps
     repo = Repo(repo_directory)
@@ -26,24 +26,23 @@ def makeBranches(repo_directory, branch_code_steps):
         commit = repo.commit(commitHash)
 
         branch_name = get_branch_name_from_commit_message(commit.message)
+        if verbose:
+            print "Branch: ", branch_name, " for commit ", commitHash
 
-        if IS_BRANCH_STRING in branch_name:
+        if test == False and IS_BRANCH_STRING in branch_name:
             if branch_name in repo.branches:
-                repo.git.branch(branch_name, "-D")
+                 repo.git.branch(branch_name, "-D")
             new_branch = repo.create_head(branch_name)
             new_branch.set_commit(commitHash)
-
-            # print "Would create: ", branch_name, " for commit ", commitHash 
             
 
 def get_branch_name_from_commit_message(message):
-    first_line = message.split("\n")[0]
+    first_line = message.splitlines()[0]
     safe_message = "".join(
-        c for c in message if c.isalnum() or c in SAFE_CHARS).strip()
+        c for c in first_line if c.isalnum() or c in SAFE_CHARS).strip()
     return (safe_message[:MAX_LENGTH]
             if len(safe_message) >
             MAX_LENGTH else safe_message)
-
 
 
 
@@ -65,11 +64,21 @@ def main():
     parser.add_argument('-b', '--branch', default=DEFAULT_STEP_BRANCH,
                         help="branch where the steps are. Default is master")
 
+    parser.add_argument('-v', '--verbose', default=False, const=True,
+			help="print out expected results. Default is false",
+                        nargs='?')
+
+    parser.add_argument('-t', '--test', default=False, const=True,
+                        help="test only. Do not create branches",
+                        nargs='?')
+
     parsed = parser.parse_args()
 
     makeBranches(
         parsed.directory,
-        parsed.branch
+        parsed.branch,
+	parsed.verbose,
+        parsed.test
         )
 
 
